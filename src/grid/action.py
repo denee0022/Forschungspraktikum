908d465d12216a60cfa768f748347e0b652c291d
Marco Schulz <marco214@t-online.de>
2025-09-05 21:30:02 +0200

    #21 minor fixes for actions and preferences

--- src/grid/action.py
+++ src/grid/action.py
@@ -17,13 +17,28 @@ class Action:
         citizen.tank_self_determination.update(-2)
         citizen.tank_food.update(-2)
 
-    def freetime_UGS(self, citizen):
-        citizen.tank_mental_health.update(4)
-        citizen.tank_physical_health.update(4)
-        citizen.tank_leisure.update(6)
-        citizen.tank_social_inclusion.update(4)
-        citizen.tank_self_determination.update(6)
-        citizen.tank_food.update(4)
+    def freetime_UGS(self, citizen, greenscore):
+        if greenscore >= 66:
+            citizen.tank_mental_health.update(4)
+            citizen.tank_physical_health.update(4)
+            citizen.tank_leisure.update(6)
+            citizen.tank_social_inclusion.update(4)
+            citizen.tank_self_determination.update(5)
+            citizen.tank_food.update(4)
+        elif 33 <= greenscore < 66:
+            citizen.tank_mental_health.update(2)
+            citizen.tank_physical_health.update(4)
+            citizen.tank_leisure.update(2)
+            citizen.tank_social_inclusion.update(3)
+            citizen.tank_self_determination.update(5)
+            citizen.tank_food.update(-6)
+        elif greenscore < 33:
+            citizen.tank_mental_health.update(-1)
+            citizen.tank_physical_health.update(4)
+            citizen.tank_leisure.update(-1)
+            citizen.tank_social_inclusion.update(2)
+            citizen.tank_self_determination.update(5)
+            citizen.tank_food.update(-6)
 
     def freetime_home(self, citizen):
         citizen.tank_mental_health.update(4)
